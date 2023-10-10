c02fae1211c0c96e4bd137e79a8cec42f2f59405
Daiki <corral-nicolas@hotmail.com>
2023-10-10 20:35:53 -0300

    Update final

--- src/components/SendOrder.jsx
+++ src/components/SendOrder.jsx
@@ -3,7 +3,7 @@ import { useState, useContext } from 'react'
 import { CartContext } from '../context/CartContext'
 import { Button } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import { Center, Heading } from '@chakra-ui/layout'
+import { Flex, Center, Heading } from '@chakra-ui/layout'
 import { FormControl, FormLabel, Input } from '@chakra-ui/react'
 
 const SendOrder = () => {
@@ -31,33 +31,50 @@ const SendOrder = () => {
 
     const orderCollection = collection(db, "orders")
 
-    const sent = orderId ? true : false
+    // const sent = orderId ? true : false
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+
+            {/* <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Nombre y Apellido" onChange={(e) => setName(e.target.value)} />
                 <input type="email" placeholder="Correo electrónico" onChange={(e) => setEmail(e.target.value)} />
                 <input type='tel' placeholder="Teléfono" onChange={(e) => setPhone(e.target.value)} />
                 <button type="submit">Confirm Order</button>
-            </form>
-            {sent &&
+            </form> */}
+            {orderId ?
                 <>
-                    <Center p={'16'}>
+                    <Flex p={'16'} direction='column-reverse' justify={'center'} align={'center'}>
                         <Heading size='xl'>
-                            The order has been submitted. Your order ID: {orderId}</Heading>
+                            The order has been submitted. Your order ID: {orderId}
+                        </Heading>
+                        <br/>
                         <Heading size='xl'>
                             Thank you for your purchase!
                         </Heading>
-                    </Center>
+                    </Flex>
                     <Center>
                         <Link to='/'>
-                            <Button size='lg'>
+                            <Button size='lg' onClick={clearCart}>
                                 Return to store
                             </Button>
                         </Link>
                     </Center>
                 </>
+                :
+                <Flex minWidth={'80rem'} direction='column-reverse' justify={'center'} align={'center'}>
+                    <form onSubmit={handleSubmit} style={{minWidth:'50rem'}}>
+                        <FormControl isRequired margin={'4'}>
+                            <FormLabel margin={'1'}>Full Name</FormLabel>
+                            <Input type='text' placeholder="John Doe" margin={'1'} onChange={(e) => setName(e.target.value)} />
+                            <FormLabel margin={'1'}>Email address</FormLabel>
+                            <Input type='email' placeholder="foo@bar.com" margin={'1'} onChange={(e) => setEmail(e.target.value)} />
+                            <FormLabel margin={'1'}>Phone Number</FormLabel>
+                            <Input type='text' placeholder="+1-202-555-0195" margin={'1'} onChange={(e) => setPhone(e.target.value)} />
+                            <Button colorScheme='green' type='submit' marginTop={'3'} marginLeft={'1'}>Confirm Order</Button>
+                        </FormControl>
+                    </form>
+                </Flex>
             }
         </>
     )
