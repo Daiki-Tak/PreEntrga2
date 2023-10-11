852afb745ea2456bc45384869299022f5f0543e4
Daiki <corral-nicolas@hotmail.com>
2023-10-11 18:13:56 -0300

    me olvidé de agregar el loader

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import { Flex } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { getFirestore, getDoc, doc } from "firebase/firestore";
+import { BeatLoader } from 'react-spinners'
 
 const ItemDetailContainer = () => {
     const { id } = useParams();
@@ -14,14 +15,22 @@ const ItemDetailContainer = () => {
         getDoc(singleProduct).then((snapshot) => {
             const docs = snapshot.data()
             const docId = snapshot.id
-            setProduct({...docs, id: docId})
+            setProduct({ ...docs, id: docId })
         })
     }, [])
 
     return (
-        <Flex justifyContent='center' gap='2' wrap='wrap'>
-            <ItemDetail product={product} key={product.id}/>
-        </Flex>
+        <>
+            {
+                Object.keys(product).length === 0 ?
+                    <div style={{ width: 'fit-content', marginLeft: 'auto', marginRight: 'auto', marginTop: '48px' }}>
+                        <BeatLoader size={16} color='gray' />
+                    </div>
+                    : <Flex justifyContent='center' gap='2' wrap='wrap'>
+                        <ItemDetail product={product} key={product.id} />
+                    </Flex>
+            }
+        </>
     )
 }
 
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import ItemList from './ItemList'
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react'
 import { collection, getDocs, getFirestore } from 'firebase/firestore'
+import { BeatLoader } from 'react-spinners'
 
 const ItemListContainer = ({ }) => {
     const { id } = useParams();
@@ -10,19 +11,25 @@ const ItemListContainer = ({ }) => {
     useEffect(() => {
         const db = getFirestore()
         const productsCollection = collection(db, 'productos')
-        getDocs(productsCollection).then((snapshot)=>{
-            const docs = snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
+        getDocs(productsCollection).then((snapshot) => {
+            const docs = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
             setProducts(docs)
         })
     }, [])
-    
+
 
     const filteredProducts = products.filter(x => !id || x.category === id);
 
     return (
-       <>
-            <ItemList products={filteredProducts} />
-       </>
+        <>
+            {
+                products.length <= 0 ?
+                    <div style={{ width: 'fit-content', marginLeft: 'auto', marginRight: 'auto', marginTop: '48px' }}>
+                        <BeatLoader size={16} color='gray' />
+                    </div>
+                    : <ItemList products={filteredProducts} />
+            }
+        </>
     )
 }
 
